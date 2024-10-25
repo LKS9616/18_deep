ea8b3ec53563455b474ca35dd21f87f50ed5d0ef
DESKTOP-RHTH2UM\80419 <lks609591@gmail.com>
2024-10-25 14:14:21 +0900

    ddddddd

---
+++ 04_external-api-request/src/main/java/com/ohgiraffers/apirequest/section01/dto/ResponseDTO.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.apirequest.section01.dto;
+
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class ResponseDTO {
+
+    private String result;
+}
